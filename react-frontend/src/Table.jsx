16f93f84515b4d3257a02b8c5f35e8e149cb93ac
Curtis Bradley <curtisbradley822@gmail.com>
2024-09-30 09:53:08 -0700

    Parts 1 & 2

---
+++ react-frontend/src/Table.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Table(){
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Job
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>
+                    Charlie
+                </td>
+                <td>
+                    Janitor
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Mac
+                </td>
+                <td>
+                    Bouncer
+                </td>
+            </tr>
+
+            </tbody>
+
+        </table>
+    )
+}
+export default Table;
--- react-frontend/src/main.jsx
+++ react-frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+import MyApp from "./MyApp";
+import Table from "./Table.jsx";
+
+
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+root.render(<MyApp />);
