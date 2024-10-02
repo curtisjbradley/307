33d8b24e1af7ddc82b9f20f19f609f03a9da8321
Curtis Bradley <curtisbradley822@gmail.com>
2024-10-02 00:40:52 -0700

    Part 5

--- react-frontend/src/MyApp.jsx
+++ react-frontend/src/MyApp.jsx
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Table from './Table';
 
 
-const characters = [
-    {
-        name: "Charlie",
-        job: "Janitor"
-    },
-    {
-        name: "Mac",
-        job: "Bouncer"
-    },
-    {
-        name: "Dee",
-        job: "Aspiring actress"
-    },
-    {
-        name: "Dennis",
-        job: "Bartender"
-    }
-];
 
 function MyApp() {
+    const [characters, setCharacters] = useState([
+        {
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            name: "Dee",
+            job: "Aspiring actress"
+        },
+        {
+            name: "Dennis",
+            job: "Bartender"
+        }
+    ]);
+    function removeOneCharacter(index) {
+        const updated = characters.filter((c,i) => i !== index);
+        setCharacters(updated);
+        console.log(index);
+    }
+
     return (
         <div className="container">
             <h1>Hello, React!</h1>
-            <Table characterData={characters} />
+            <Table characterData={characters}
+            removeCharacter={removeOneCharacter} />
 
         </div>
     );
 }
+
+
 export default MyApp;
