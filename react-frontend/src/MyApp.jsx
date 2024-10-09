c73eac95abddf225c02ee923fe6067fb011696fb
Curtis Bradley <curtisbradley822@gmail.com>
2024-10-09 09:43:12 -0700

    Working Fetch

--- react-frontend/src/MyApp.jsx
+++ react-frontend/src/MyApp.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
 
@@ -6,25 +6,42 @@ import Form from './Form';
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetchUsers().then(res => res.json())
+            .then((json) => setCharacters(json["users_list"]))
+            .catch((error) => {
+                console.log(error)})
+    }, []);
+
     function removeOneCharacter(index) {
-        const updated = characters.filter((c,i) => i !== index);
+        const updated = characters.filter((c, i) => i !== index);
         setCharacters(updated);
         console.log(index);
     }
 
     function updateList(person) {
-        setCharacters([...characters,person]);
+        setCharacters([...characters, person]);
     }
 
     return (
         <div className="container">
             <h1>Hello, React!</h1>
             <Table characterData={characters}
-            removeCharacter={removeOneCharacter} />
-            <Form handleSubmit={updateList} />
+                   removeCharacter={removeOneCharacter}/>
+            <Form handleSubmit={updateList}/>
         </div>
     );
+
+
+
+
+    async function fetchUsers() {
+        const promise = await fetch("http://localhost:8000/users")
+        console.log(promise)
+        return promise
+    }
+
+
 }
-
-
 export default MyApp;
